c7858f0a5844c0dabba87ba137dda3a3db36b004
ShenChang618 <s2696922797c@outlook.com>
2022-06-08 14:08:16 +0800

    fix(@loong-js/react-mobx): createBind not observable paramater
    affects: @loong-js/react-mobx

--- packages/react-mobx/src/bind.ts
+++ packages/react-mobx/src/bind.ts
@@ -1,9 +1,8 @@
-import { observable, autorun } from 'mobx';
+import { autorun } from 'mobx';
 import { observer } from 'mobx-react-lite';
 import { createBind } from '@loong-js/react-pure';
 
 export const bind = createBind({
-  observable,
   view: observer,
   observe: (observeFunction) => {
     const disposer = autorun(observeFunction);
