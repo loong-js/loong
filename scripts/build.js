92adb81a828bca31a12b82dbdc7458e08ce1cf75
ShenChang618 <s2696922797c@outlook.com>
2022-07-22 14:00:39 +0800

    fix(all): optimization package export
    affects: @loong-js/component, @loong-js/core, @loong-js/observer, @loong-js/react-mobx,
    @loong-js/react-pure, @loong-js/react, @loong-js/shared

--- scripts/build.js
+++ scripts/build.js
@@ -5,20 +5,21 @@ const typescript = require('@rollup/plugin-typescript');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const { default: dts } = require('rollup-plugin-dts');
+const { terser } = require('rollup-plugin-terser');
 const ora = require('ora');
 const chalk = require('chalk');
 
 const PACKAGES_PATH = path.join(__dirname, '../packages');
 const PACKAGES_NAMES = ['shared', 'core', 'component', 'observer', 'react-pure', 'react-mobx', 'react'];
 const indexTypeDtsList = [];
-
-/**
- * package
- *
- * example
- */
-const external = ['react', 'mobx', 'mobx-react-lite', 'reflect-metadata'];
-const outputs = ['cjs', 'esm', 'umd'];
+const external = [
+  'react',
+  'mobx',
+  'mobx-react-lite',
+  'reflect-metadata',
+  ...PACKAGES_NAMES.map(name => `@loong-js/${name}`),
+];
+const outputs = ['cjs', 'cjs.min', 'esm', 'umd', 'umd.min'];
 
 async function buildEntry(name) {
   const PACKAGE_PATH = path.join(PACKAGES_PATH, name);
@@ -40,12 +41,27 @@ async function buildEntry(name) {
   });
   const packageConfig = require(path.join(PACKAGE_PATH, 'package.json'));
 
-  await Promise.all(outputs.map(output => bundle.write({
-    file: path.join(PACKAGE_PATH, 'dist', `index.${output}.js`),
-    format: output,
-    name: packageConfig.name,
-    exports: 'named'
-  })));
+  fs.writeFileSync(
+    path.join(PACKAGE_PATH, 'dist', 'index.js'),
+    `
+if (typeof process !== 'undefined' && process.env.NODE_ENV === 'production') {
+  module.exports = require('./index.cjs.min.js');
+} else {
+  module.exports = require('./index.cjs.js');
+}
+    `.trim(),
+  );
+
+  await Promise.all(outputs.map(output => {
+    const [format, min] = output.split('.');
+    return bundle.write({
+      format,
+      file: path.join(PACKAGE_PATH, 'dist', `index.${output}.js`),
+      name: packageConfig.name,
+      exports: 'named',
+      plugins: [!!min && terser()],
+    });
+  }));
 }
 
 
